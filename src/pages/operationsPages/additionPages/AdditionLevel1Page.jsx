a3c1a9e4eb9547f79fee08cb31601ddba333ba15
asdf123dd <vedanth.ramji@outlook.com>
2021-11-04 14:28:33 +0530

    finished level page template, need to style it

--- src/pages/operationsPages/additionPages/AdditionLevel1Page.jsx
+++ src/pages/operationsPages/additionPages/AdditionLevel1Page.jsx
@@ -1,51 +1,7 @@
-import {useState} from 'react';
-import SubHeader from '../../../components/SubHeader';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import LevelPageTemplate from '../../../templates/LevelPageTemplate';
 
 export default function AdditionLevel1Page() {
-    const [questionNum, setQuestionNum] = useState(1);
-    const [input, setInput] = useState('');
-    const [num1, setNum1] = useState(Math.floor(Math.random() * 100));
-    const [num2, setNum2] = useState(Math.floor(Math.random() * 100));
-
-    const handleSubmit = () => {
-        if (input === '' || isNaN(input)) {
-            alert('Please enter a numeric value');
-        } else if (parseInt(input) !== num1 + num2) {
-            alert('Oops... Your answer is wrong!');
-        } else {
-            alert('Yay! Your answer is correct!');
-
-            setNum1(Math.floor(Math.random() * 100));
-            setNum2(Math.floor(Math.random() * 100));
-            setQuestionNum(questionNum + 1);
-        }
-    }
-
     return (
-        <div>
-            <SubHeader title='Addition | Level 1' link='/practice/addition' />
-            Question {questionNum}
-
-            <div style={{textAlign: 'center'}}>
-                {num1} + {num2}
-            </div>
-
-            <TextField
-                id='filled-basic'
-                label='Enter Number'
-                variant='outlined'
-                onChange={e => setInput(e.target.value)}
-            />
-
-            <Button
-                variant='contained'
-                style={{backgroundColor: '#3DB2FF'}}
-                onClick={handleSubmit}
-            >
-                Submit
-            </Button>
-        </div>
+        <LevelPageTemplate level='Level 1' operationName='Addition' operationSymbol='+' />
     );
 }
---
+++ src/templates/LevelPageTemplate.jsx
@@ -0,0 +1,77 @@
+import {useState} from 'react';
+import SubHeader from '../components/SubHeader';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import '../styles/GlobalStyles.css';
+
+export default function LevelPageTemplate({level, operationName, operationSymbol}) {
+    let scale = 0;
+    switch(level) {
+        case 'Level 1':
+            scale = 10;
+            break;
+        case 'Level 2':
+            scale = 100;
+            break;
+        default:
+            scale = 1000;
+    }
+
+    const [num1, setNum1] = useState(Math.floor(Math.random() * scale));
+    const [num2, setNum2] = useState(Math.floor(Math.random() * scale));
+    const [questionNum, setQuestionNum] = useState(1);
+    const [input, setInput] = useState('');
+
+    const checkAnswer = (input, target) => {
+        if (parseInt(input) !== target) {
+            alert('Your answer is wrong!');
+            return;
+        }
+
+        alert('Your answer is correct!');
+        setNum1(Math.floor(Math.random() * scale));
+        setNum2(Math.floor(Math.random() * scale));
+        setQuestionNum(questionNum + 1);
+    }
+
+    const handleSubmit = () => {
+        if (input === '' || isNaN(input)) {
+            alert('Please enter a numeric value');
+            return;
+        }
+
+        switch (operationName) {
+            case 'Addition':
+                checkAnswer(input, num1 + num2);
+                break;
+            case 'Subtraction':
+                checkAnswer(input, num1 - num2);
+                break;
+            case 'Multiplication':
+                checkAnswer(input, num1 * num2);
+                break;
+            default:
+                checkAnswer(input, num1 / num2);
+        }
+    }
+
+    return (
+        <div className='levelPageTemplate'>
+            <SubHeader title={`${operationName} | ${level}`} link={`/practice/${operationName.toLowerCase()}`} />
+            <h3 className='closedSubheader'>Question {questionNum}: {num1} + {num2}</h3>
+            <TextField
+                id='filled-basic'
+                label='Enter Number'
+                variant='outlined'
+                onChange={e => setInput(e.target.value)}
+            />
+            <Button
+                variant='contained'
+                style={{backgroundColor: '#3DB2FF'}}
+                onClick={handleSubmit}
+            >
+                Submit
+            </Button>
+        </div>
+    );
+}
